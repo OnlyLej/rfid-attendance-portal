eb74f433e322e6ca18671289198dc6a680779730
Lej <120243069+OnlyLej@users.noreply.github.com>
2026-02-23 21:34:44 +0800

    Add sitemap function for site URL and metadata

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://rfid-attendance-portal.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
+    },
+  ];
+}
